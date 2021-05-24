652a59b40ef47e8779b1002dca5269daf44d9291
jerabekjak <jerabekjaku@gmail.com>
2021-05-24 19:26:55 +0200

    readcsv with scenariosa

--- practices.py
+++ practices.py
@@ -5,6 +5,24 @@ class Practices:
         self.__line = parser.line
         self.__out_dir = parser.output_dir
 
+
     def run(self):
-        pass
+        table = self.read_csv()
+
+        for i in range(1,len(table)):
+            (table[i][4]
+            
+
+
+
+
+    def read_csv(self):
+        with open(self.__tsc_path, 'r') as file_:
+            lines = file_.readlines()
+        
+        csv = []
+        for line in lines:
+            csv.append(line.replace('\n', '').split(','))
+        
+        return(csv)
 
