9aad1c0401cba015354d969375c55d6188cbb1ed
jerabekjak <jerabekjaku@gmail.com>
2021-05-24 19:10:36 +0200

    prepare constructor

--- practices.py
+++ practices.py
@@ -1,5 +1,10 @@
 class Practices:
-   def __init__(self, parser):
-       print (parser)
-      
+    def __init__(self, parser):
+        self.__tsc_path = parser.table_scenarios
+        self.__bm_path = parser.benchmark_model
+        self.__line = parser.line
+        self.__out_dir = parser.output_dir
+
+    def run(self):
+        pass
 
