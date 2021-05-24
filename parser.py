3290de1bbc245a560c982470bf25537c2d4e3cce
jerabekjak <jerabekjaku@gmail.com>
2021-05-24 18:48:22 +0200

    add parser file

---
+++ parser.py
@@ -0,0 +1,35 @@
+import argparse
+import textwrap
+
+def read_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+        Run scenarios of varous management practices
+         '''))
+
+    parser.add_argument(
+        '-tsc',
+        '--table_scenarios',
+        help='csv table where the scenarios are specified',
+        type=str,
+        required=True
+    )
+
+    parser.add_argument(
+        '-bm',
+        '--benchmark_model',
+        help='prepared H1D benchmark model',
+        type=str,
+        required=True
+    )
+
+    parser.add_argument(
+        '-line',
+        '--line',
+        help='line where the parameters has to be changed',
+        type=str,
+        required=True
+    )
+
+    return parser.parse_args()
