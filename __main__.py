1ed49bc8ea9a2a9d4f98108b9da59995ad4b36e6
jerabekjak <jerabekjaku@gmail.com>
2021-05-24 19:27:33 +0200

    add practices class in main

--- __main__.py
+++ __main__.py
@@ -1,3 +1,8 @@
-import adsf
-print (__name__)
-print ('adsf')
+#!/usr/bin/python3
+import parser
+from practices import Practices
+
+
+if __name__ == '__main__':
+    sens = Practices(parser.read_parser())
+    sens.run()
